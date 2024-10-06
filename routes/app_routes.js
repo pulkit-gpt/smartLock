59328ca3af7996ee3af81d5e3d4c9acb3c992d65
pulkit-gpt <hiiampulkitg@gmail.com>
2024-10-07 00:16:39 +0530

    added router to app_controller

---
+++ routes/app_routes.js
@@ -0,0 +1,35 @@
+import express from "express";
+const router = express.Router();
+
+import {
+  add_user,
+  remove_user_auth,
+  timed_user,
+  open_door,
+} from "../controllers/app_controllers";
+
+router.post("/add_new_user", (req, res) => {
+  console.log("add_user");
+  console.log(req.body);
+  add_user(req, res);
+});
+
+router.post("/remove_user_auth", (req, res) => {
+  console.log("remove_user");
+  console.log(req.body);
+  remove_user_auth(req, res);
+});
+
+router.post("/timed_user", (req, res) => {
+  console.log("timed_user");
+  console.log(req.body);
+  timed_user(req, res);
+});
+
+router.post("/open_door", (req, res) => {
+  console.log("open_door");
+  console.log(req.body);
+  open_door(req, res);
+});
+
+export default router;
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
 import express from "express";
 import data_routes from "./data_routes.js";
+import app_routes from "./app_routes.js";
+
 const router = express.Router();
+
 router.use("/data", data_routes);
+router.use("/app", app_routes);
