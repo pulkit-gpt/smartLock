4d51184b2d55ba5972c67e958b5bc675ae6e64f2
pulkit-gpt <hiiampulkitg@gmail.com>
2024-10-06 18:46:43 +0530

    added index file to route

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,4 @@
 import express from "express";
+import data_routes from "./data_routes.js";
 const router = express.Router();
-import {
-  get_data,
-  create_data,
-  update_data,
-  remove_data,
-} from "../controllers/data_controller.js";
-
-router.get("/get_data", (req, res) => {
-  console.log("get_data");
-  console.log(req.body);
-  res = get_data(req, res);
-});
-
-router.post("/create_data", (req, res) => {
-  console.log("create_data");
-  console.log(req.body);
-  res = create_data(req, res);
-});
-
-router.post("/update_data", (req, res) => {
-  console.log("update_data");
-  console.log(req.body);
-  res = update_data(req, res);
-});
-
-router.post("/remove_data", (req, res) => {
-  console.log("remove_data");
-  console.log(req.body);
-  res = remove_data(req, res);
-});
-
-export default router;
+router.use("/data", data_routes);
